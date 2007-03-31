baa1c18f8f2c7a84b30ab0ea00aef1d0e05dbde6
wiseleyb <wiseleyb@3b581b49-eb29-0410-8f35-9d9d98fae791>
2007-03-31 21:33:04 +0000

    add sample project and plugin

---
+++ test/unit/rss_entry_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class RssEntryTest < Test::Unit::TestCase
+  fixtures :rss_entries
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
